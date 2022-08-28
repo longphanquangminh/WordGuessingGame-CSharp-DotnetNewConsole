7bc7a483ab00ac8bed370e02698d1237b7d33969
Phan Quang Minh Long <lordaizensousuke2001@gmail.com>
2022-08-28 11:49:44 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace WordGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            string[] words = {"dog", "cat", "boy", "girl"};
+            string wordToGuess = words[rand.Next(0, words.Length)];
+
+            bool hasWon = false;
+
+            for(int i = 0; i<words.Length/2; i++){
+                Console.WriteLine("Guess The Word: ");
+
+                for(int j = 0; j < words.Length; j++){
+                    Console.WriteLine(words[j] + " ");
+                }
+
+                Console.WriteLine();
+
+                string guess = Console.ReadLine();
+
+                if(guess == wordToGuess){
+                    hasWon = true;
+                    break;
+                }
+            }
+            if(hasWon){
+                Console.WriteLine("You've won!");
+            }
+            else{
+                Console.WriteLine("You've lost! The word was " + wordToGuess);
+            }
+        }
+    }
+}
